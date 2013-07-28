638eb3adce975e104e104cd68cb32a37caa86e20
ronghao100 <ronghao100@gmail.com>
2013-07-28 15:49:45 +0800

    [ronghao]add splash screen

---
+++ daifan51Project/daifan51/src/main/java/com/daifan/activity/SplashScreenActivity.java
@@ -0,0 +1,42 @@
+package com.daifan.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.daifan.R;
+
+/**
+ * Created by ronghao on 13-7-28.
+ * load logo when startup
+ */
+public class SplashScreenActivity extends Activity {
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, PostListActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
